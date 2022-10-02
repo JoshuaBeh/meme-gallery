ce295cc85763a55dc7c4c5d432c47c84cd48a564
Joshua Beh <joshbeh88@gmail.com>
2022-10-02 05:07:51 +0000

    completed meme gallery project

---
+++ main.js
@@ -0,0 +1,24 @@
+var $gallery = document.querySelector('main');
+var imageUrls = [];
+var $imageUrlInput = document.querySelector('input');
+var $addImageButton = document.querySelector('button');
+
+$addImageButton.addEventListener('click', function () {
+  if ($imageUrlInput.value !== '') {
+    imageUrls.push($imageUrlInput.value);
+    $imageUrlInput.value = '';
+    updateGallery();
+  }
+});
+
+var updateGallery = function () {
+  $gallery.innerHTML = '';
+  for (let i = 0; i < imageUrls.length; i++) {
+    var $imageElement = document.createElement('img');
+    $imageElement.className = 'gallery-image';
+    $imageElement.src = imageUrls[i];
+    $gallery.appendChild($imageElement);
+  }
+};
+
+updateGallery();
